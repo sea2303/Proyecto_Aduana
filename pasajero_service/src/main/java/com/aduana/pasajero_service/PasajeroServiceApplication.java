8aaedb7c4e8868f28bca1b4d51c9345d84661f9a
TheTomasXDD <tom.nunezb@duocuc.cl>
2026-06-10 15:51:46 -0400

    Se agrego el microservicio Pasajero

---
+++ pasajero_service/src/main/java/com/aduana/pasajero_service/PasajeroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aduana.pasajero_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasajeroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasajeroServiceApplication.class, args);
+	}
+
+}
---
+++ pasajero_service/src/test/java/com/aduana/pasajero_service/PasajeroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aduana.pasajero_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasajeroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
