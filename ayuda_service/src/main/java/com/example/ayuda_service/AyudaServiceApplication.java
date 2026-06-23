d8f2d3dcf69fe3520e3ed3cdc112b801e51a8ee6
TheTomasXDD <tom.nunezb@duocuc.cl>
2026-06-23 09:22:17 -0400

    se cambiaron los puertos

--- ayuda_service/src/main/java/com/example/ayuda_service/AyudaServiceApplication.java
+++ ayuda_service/src/main/java/com/example/ayuda_service/AyudaServiceApplication.java
@@ -1,35 +1,12 @@
 package com.example.ayuda_service;
 
-import com.example.ayuda_service.model.Ayuda;
-import com.example.ayuda_service.repository.AyudaRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AyudaServiceApplication implements CommandLineRunner {
-
-	private final AyudaRepository repository;
-
-	public AyudaServiceApplication(AyudaRepository repository) {
-		this.repository = repository;
-	}
+public class AyudaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AyudaServiceApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) {
-
-		if(repository.count() == 0){
-
-			Ayuda ayuda = new Ayuda();
-
-			ayuda.setTitulo("Ingreso de Vehículos");
-			ayuda.setDescripcion("Debe presentar padrón, licencia y seguro internacional.");
-
-			repository.save(ayuda);
-		}
 	}
 }
