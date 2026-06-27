7a44690fbbfa564d4d24d8696a0f442b8ac4502f
TheTomasXDD <tom.nunezb@duocuc.cl>
2026-06-26 23:14:27 -0400

    Se agrego el microservicio-reportes

---
+++ reportes-service/src/main/java/com/example/reportes_service/ReportesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reportes_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportesServiceApplication.class, args);
+	}
+
+}
