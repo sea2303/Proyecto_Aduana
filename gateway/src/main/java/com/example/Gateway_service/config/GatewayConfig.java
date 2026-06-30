081481d804187e9d382877f4a7f6672faf7e228f
TheTomasXDD <tom.nunezb@duocuc.cl>
2026-06-30 01:26:42 -0400

    .

--- gateway/src/main/java/com/example/Gateway_service/config/GatewayConfig.java
+++ gateway/src/main/java/com/example/Gateway_service/config/GatewayConfig.java
@@ -11,12 +11,10 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                // 🚗 PARCHEO DE RUTA: Recibe "/api/vehiculos" y lo mapea al controlador real con "/api/v1/vehiculos"
                 .route("vehiculo-service-route", r -> r.path("/vehiculo-service/api/vehiculos/**")
                         .filters(f -> f.setPath("/api/v1/vehiculos"))
                         .uri("lb://VEHICULO-SERVICE"))
 
-                // 🔐 RUTA PARA AUTENTICACIÓN
                 .route("servicio-autenticacion-route", r -> r.path("/servicio-autenticacion/**")
                         .filters(f -> f.rewritePath("/servicio-autenticacion/(?<remaining>.*)", "/${remaining}"))
                         .uri("lb://SERVICIO-AUTENTICACION"))
